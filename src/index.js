4c553e4e6d2cb492e8214f1b6592d38a5d7acbef
TiieuTiien <phamquoctien1903@gmail.com>
2025-04-08 21:06:38 +0700

    Resolve merge conflict

--- src/index.js
+++ src/index.js
@@ -1,69 +1,17 @@
-import ArrayStructure from './data-structures/Array.js';
-import DragDrop from './drag-drop/drag-drop.js'; // Import the DragDrop class
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-document.addEventListener("DOMContentLoaded", () => {
-  const startButton = document.getElementById("startButton");
-  const insertButton = document.getElementById("insertButton");
-  const arrayContainer = document.getElementById("arrayContainer");
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-  let arrayStructure = new ArrayStructure(30); // Create an instance of the array with 20 elements
-  let array = arrayStructure.getArray(); // Get the array from the ArrayStructure class
-
-  // Declare a persistent variable for dragDrop.
-  let dragDropInstance;
-
-  // Function to render the array as bars
-  function renderArray(arr, highlightIndices = null) {
-    arrayContainer.innerHTML = ''; // Clear the previous array content
-  
-    arr.forEach((value, index) => {
-      const bar = document.createElement('div');
-      bar.classList.add('bar');
-      
-      if (highlightIndices !== null) {
-        if (Array.isArray(highlightIndices)){
-          if (highlightIndices.includes(index)) {
-            bar.classList.add('newly-added');
-          }
-        } else if (index === highlightIndices) {
-          bar.classList.add("newly-added");
-        }
-      }
-      
-      bar.style.height = `${value * 3}px`; // Height of the bar
-      bar.style.width = '30px'; // Width of the bar
-      bar.style.margin = '0 5px'; // Margin between bars
-      bar.dataset.index = index; // Store the index of the array element on the bar
-      arrayContainer.appendChild(bar);
-    });
-  
-    if (!dragDropInstance) {
-      dragDropInstance = new DragDrop(arrayContainer, arr, renderArray);
-    } else {
-      dragDropInstance.array = arr;
-      dragDropInstance.enableDragAndDrop();
-    }
-  }
-
-  renderArray(array); // Initial rendering of the array
-
-  // Event listener for the "Start Sorting" button
-  startButton.addEventListener("click", async () => {
-    // Pass renderArray as the callback so it gets called after each swap.
-    await arrayStructure.bubbleSortAnimated(arrayStructure.getArray(), renderArray);
-
-    renderArray(array);
-  });
-  
-  insertButton.addEventListener("click", () => {
-    const randomIndex = Math.floor(Math.random() * (array.length + 1));
-    const randomValue = Math.floor(Math.random() *  100) + 1;
-    arrayStructure.insertAt(randomIndex, randomValue);
-
-    // Update the predefined array
-    array = arrayStructure.getArray();
-
-    renderArray(array, randomIndex);
-  });
-
-});
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
